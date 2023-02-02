48a16ab7d23fbc8edd0631f2666690c8096581a1
k-vernon <kvernon908@gmail.com>
2023-02-01 21:51:31 -0500

    put details links inside div

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -8,8 +8,11 @@ const ProjectDetails = () => {
       <h1>{location.state.title}</h1>
       <p>{location.state.description}</p>
       <img src={location.state.image} alt={location.state.title} />
-      <a href={location.state.repositoryLink}>Github Repo</a>
-      <a href={location.state.deploymentLink}>Deployment Link</a>
+      <div>
+        <a href={location.state.repositoryLink}>Github Repo</a>
+        <a href={location.state.deploymentLink}>Deployment Link</a>
+      </div>
+
     </>
   )
 }
