bc25e730703434b7938e829107192f8e41de3ee6
k-vernon <kvernon908@gmail.com>
2023-02-01 22:35:33 -0500

    deploy app

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,15 +1,11 @@
-import DownloadLink from "react-download-link"
+
 
 function Resume() {
   return (
     <>
       <h1>Resume</h1>
       <p> Click the link to download my resume!</p>
-      <DownloadLink 
-      label="Download" 
-      filename="Kiana's Resume"
-      exportFile={() => "My cached data"}
-      />
+      <a href="/Resume/Resume.pdf">Download</a>
     </>
   )
 }
