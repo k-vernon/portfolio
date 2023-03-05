77fd9ab44940f55ad2129a2d51654f535961e408
k-vernon <kvernon908@gmail.com>
2023-03-05 00:41:00 -0500

    add header

--- src/App.js
+++ src/App.js
@@ -10,11 +10,14 @@ import ProjectDetails from './pages/ProjectDetails'
 
 // COMPONENTS 
 import NavBar from './components/NavBar'
-
+import Header from './components/Header'
 
 function App() {
   return (
     <>
+    <>
+    <Header/>
+    </>
     <NavBar />
       <Routes>
         <Route path="/" element={< Home />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./header.css"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">Vernon</a>
+
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="" className="nav__link"></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
