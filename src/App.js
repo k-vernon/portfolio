504a3826f3b92837b4bc9be60e02808b1d638937
k-vernon <kvernon908@gmail.com>
2023-03-06 05:36:20 -0500

    add skills and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import { Routes, Route } from 'react-router-dom'
 import "./App.css"
 
-// PAGES
-
-
 // COMPONENTS 
-import Header from './components/Header'
-import Home from './components/Home/Home'
+import Header from './components/Header';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
     <>
-    <Header/>
-    <main className='main'>
-      <Home/>
-    </main>
-      {/* <Routes>
-        <Route path="/" element={< Home />} />
-      </Routes> */}
+      <Header />
+      <main className='main'>
+        <Home />
+        <About />
+        <Skills />
+      </main>
     </>
   );
 }
