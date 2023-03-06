a54f4337a12301a942ffbd4de6bfcc578787ae52
k-vernon <kvernon908@gmail.com>
2023-03-06 01:23:07 -0500

    add links, photo, styling

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { Routes, Route } from 'react-router-dom'
 import "./App.css"
 
 // PAGES
-import Home from './pages/Home'
+
 
 // COMPONENTS 
 import Header from './components/Header'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <>
     <Header/>
+    <main className='main'>
+      <Home/>
+    </main>
       {/* <Routes>
         <Route path="/" element={< Home />} />
       </Routes> */}
---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.linkedin.com/in/kiana-vernon-a05670184/" className="home__social-icon" rel='noreferrer' target="_blank">
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+
+      <a href="https://github.com/k-vernon" className="home__social-icon" rel='noreferrer' target="_blank">
+        <i class="uil uil-github-alt"></i>
+      </a>
+
+      <a href="mailto:kvernon908@gmail.com" className="home__social-icon" rel='noreferrer' target="_blank">
+        <i class="uil uil-envelope"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
