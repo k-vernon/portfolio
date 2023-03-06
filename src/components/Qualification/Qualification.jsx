0a3a6524728abf12436ae1f0a0070a3db05a76b4
k-vernon <kvernon908@gmail.com>
2023-03-06 08:42:32 -0500

    stub up qualifications

--- src/components/Qualification/Qualification.jsx
+++ src/components/Qualification/Qualification.jsx
@@ -9,7 +9,34 @@ const Qualification = () => {
 
       <div className="qualification__container container">
         <div className="qualification__tabs">
+          <div className="qualification__button qualification__active button--flex">
+            <i className="uil uil-graduation-cap qualification__icon"></i>{" "} 
+            Education
+          </div>
+
           <div className="qualification__button button--flex">
+            <i className="uil uil-briefcase-alt qualification__icon"></i>{" "} 
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div className="qualification__content">
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title"></h3>
+                <span className="qualification__subtitle">Spain - Institute</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <div className="qualification__line"></div>
+              </div>
+            </div>
+
             
           </div>
         </div>
