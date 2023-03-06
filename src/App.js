1c8efbd1c71e1840b9fee858e9909b84c93dd577
k-vernon <kvernon908@gmail.com>
2023-03-06 06:55:58 -0500

    add services section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Skills from "./components/Skills/Skills";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Services />
       </main>
     </>
   );
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,152 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section className="services section" id="services">
+      <h2 className="section__title">Services</h2>
+      <span className="section__subtitle">What I Offer</span>
+
+      <div className="sevices__container container grid">
+        <div className="services__content">
+          <div>
+            <i className="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">Product <br/> Designer</h3>
+          </div>
+
+          <span className="services__button">View More{" "} <i className="uil uil-arrow-right services__button-icon"></i></span>
+
+          <div className="services__model">
+            <div className="services__model-content">
+              <i className="uil uil-times services__model-close"></i>
+
+              <h3 className="services__model-title"></h3>
+              <p className="services__model-description">Service with over 6 years of experience. Providing quality work to clients and companies.</p>
+
+              <ul className="services__model-services grid">
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">I develop the user interface.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">Web page development.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">I create ux element interaction.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">I position your company brand.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">Design and mockups of products for companies.</p>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </div>
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-arrow services__icon"></i>
+            <h3 className="services__title">Graphic <br/> Designer</h3>
+          </div>
+
+          <span className="services__button">View More{" "} <i className="uil uil-arrow-right services__button-icon"></i></span>
+
+          <div className="services__model">
+            <div className="services__model-content">
+              <i className="uil uil-times services__model-close"></i>
+
+              <h3 className="services__model-title"></h3>
+              <p className="services__model-description">Service with over 6 years of experience. Providing quality work to clients and companies.</p>
+
+              <ul className="services__model-services grid">
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">I develop the user interface.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">Web page development.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">I create ux element interaction.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">I position your company brand.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">Design and mockups of products for companies.</p>
+                </li>
+              </ul>
+              
+            </div>
+          </div>
+        </div>
+
+        <div className="services__content">
+          <div>
+            <i className="uil uil-edit services__icon"></i>
+            <h3 className="services__title">UI/UX <br/> Designer</h3>
+          </div>
+
+          <span className="services__button">View More{" "} <i className="uil uil-arrow-right services__button-icon"></i></span>
+
+          <div className="services__model">
+            <div className="services__model-content">
+              <i className="uil uil-times services__model-close"></i>
+
+              <h3 className="services__model-title"></h3>
+              <p className="services__model-description">Service with over 6 years of experience. Providing quality work to clients and companies.</p>
+
+              <ul className="services__model-services grid">
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">I develop the user interface.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">Web page development.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">I create ux element interaction.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">I position your company brand.</p>
+                </li>
+
+                <li className="services__model-service">
+                  <i className="uil uil-check-circle services__model-icon"></i>
+                  <p className="services__model-info">Design and mockups of products for companies.</p>
+                </li>
+              </ul>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Services
