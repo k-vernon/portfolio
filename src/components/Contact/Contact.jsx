9e54ecca998b22f3b9cbd7bde010d6c5fbd3dd33
k-vernon <kvernon908@gmail.com>
2023-03-07 01:16:10 -0500

    contact form api

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import './contact.css'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+    .sendForm(
+        'service_yaxqqy2', 
+        'template_fbqwbvj', 
+        form.current, 
+        '279aG6fzfdbl_pB67'
+      )
+      e.target.reset()
+  };
+
   return (
     <section className="contact section" id="contact">
       <h2 className="section__title">Get in touch</h2>
@@ -53,7 +69,7 @@ const Contact = () => {
         <div className="contact__content">
           <h3 className="contact__title">Write me your project</h3>
 
-          <form className="contact__form">
+          <form ref={form} onSubmit={sendEmail}className="contact__form">
             <div className="contact__form-div">
               <label htmlFor="" className="contact__form-tag">Name</label>
               <input 
