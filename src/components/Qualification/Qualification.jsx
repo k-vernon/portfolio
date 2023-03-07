4535b3613a2871f7f917bd81030f0036921f5dce
k-vernon <kvernon908@gmail.com>
2023-03-06 21:28:04 -0500

    add qualification toggle

--- src/components/Qualification/Qualification.jsx
+++ src/components/Qualification/Qualification.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './qualification.css'
 
 const Qualification = () => {
+  const [toggleState, setToggleState] = useState(0)
+
+  const toggleTab = (index) => {
+    setToggleState(index)
+  }
+
   return (
     <section className="qualification section">
       <h2 className="section__title">Qualification</h2>
@@ -9,35 +15,134 @@ const Qualification = () => {
 
       <div className="qualification__container container">
         <div className="qualification__tabs">
-          <div className="qualification__button qualification__active button--flex">
-            <i className="uil uil-graduation-cap qualification__icon"></i>{" "} 
+          <div className={toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
+          onClick={() => toggleTab(1)}>
+            <i className="uil uil-graduation-cap qualification__icon"></i>
             Education
           </div>
 
-          <div className="qualification__button button--flex">
-            <i className="uil uil-briefcase-alt qualification__icon"></i>{" "} 
+          <div className={toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}
+          onClick={() => toggleTab(2)}>
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
             Experience
           </div>
         </div>
 
         <div className="qualification__sections">
-          <div className="qualification__content">
+          <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content" }>
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title"></h3>
-                <span className="qualification__subtitle">Spain - Institute</span>
+                <h3 className="qualification__title">General Assembly</h3>
+                <span className="qualification__subtitle">Remote - Bootcamp</span>
                 <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - Present
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
                 </div>
               </div>
 
               <div>
                 <span className="qualification__rounder"></span>
-                <div className="qualification__line"></div>
+                <span className="qualification__line"></span>
               </div>
             </div>
 
-            
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Content Creator</h3>
+                <span className="qualification__subtitle">Twitch </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2020 - 2022
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Graphic Design</h3>
+                <span className="qualification__subtitle">Remote - Freelance</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2017 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Design and Media Tech</h3>
+                <span className="qualification__subtitle">Marietta - Institute</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2017 - 2019
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content" }>
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">General Assembly</h3>
+                <span className="qualification__subtitle">Remote - Bootcamp</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Content Creator</h3>
+                <span className="qualification__subtitle">Twitch </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2020 - 2022
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Graphic Design</h3>
+                <span className="qualification__subtitle">Remote - Freelance</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2017 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
