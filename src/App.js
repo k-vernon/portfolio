fe5f3e4f5058525703a55ecc209d068f1325cde0
k-vernon <kvernon908@gmail.com>
2023-03-07 02:23:50 -0500

    add footer and scrollup function

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Skills from "./components/Skills/Skills";
 import Services from "./components/Services/Services";
 import Qualification from "./components/Qualification/Qualification";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import ScrollUp from "./components/ScrollUp/ScrollUp";
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
         <Qualification />
         <Contact />
       </main>
+        <Footer />
+        <ScrollUp />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Vernon</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about"
+             className="footer__link">About</a>
+          </li>
+
+          <li>
+            <a href="#skills"
+             className="footer__link">Skills</a>
+          </li>
+
+          <li>
+            <a href="#portfolio"
+             className="footer__link">Portfolio</a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a 
+          href="https://www.linkedin.com/in/kiana-vernon-a05670184/" className="footer__social-link" 
+          rel='noreferrer' 
+          target="_blank"
+          >
+            <i class='bx bxl-linkedin' ></i>
+          </a>
+
+          <a 
+          href="https://github.com/k-vernon" className="footer__social-link" 
+          rel='noreferrer' 
+          target="_blank"
+          >
+          <i class='bx bxl-github' ></i>
+          </a>
+
+          <a 
+          href="mailto:kvernon908@gmail.com" className="footer__social-link" 
+          rel='noreferrer' 
+          target="_blank"
+          >
+            <i class='bx bx-envelope' ></i>
+          </a>
+        </div>
+
+        <span className="footer__copy"> Inpired by CrypticalCoder<br/>&#169; Kiana Vernon. All Rights Reserved</span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
